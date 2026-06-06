62962855c2ba37d56b4204c957bd3d0bbef26f27
PaintBucketCard <troymations@gmail.com>
2026-06-05 20:48:20 -0400

    battle_character is adapts
    Adapted to new split from battle_entity

--- scripts/battle_entity_class.gd
+++ scripts/battle_entity_class.gd
@@ -1,12 +1,41 @@
-class BattleEntity
+class_name BattleEntity
 extends Node2D
 
+@export var battle_data: statsResource;
+@export var battle_text: quotesResource;
+@export var defensive_skill: BattleSkill;
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var entity_sprite_sheet: AnimatedSprite2D;
+@export var animation_effects: AnimationPlayer;
+@export var dialouge: Label;
+# @export var rolling_visual: PackedScene;
 
+var skill_chosen:BattleSkill;
+var targets = [];
+var is_defending: bool = false;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _get_battle_data():
+	return battle_data
+
+func _battle_phase_setup()->void:
+	targets = [];
+	is_defending = false;
+
+func _is_battle_ready()-> bool:
+	if (((self.skill_chosen != null) and !self.targets.is_empty()) or is_defending):
+		return true
+	return false
+
+func _execute_skill()->void:
+	for hit in range(skill_chosen.hit_count):
+		var stat_choice = battle_data.source_stat_array[skill_chosen.stat_power];
+		var damage = skill_chosen._getDamageAmount(stat_choice);
+		for targeting in targets:
+			targets._change_hp(damage)
+			print(targets.stat_current_hp)
+
+func _change_hp(amount:int)->void: 
+	battle_data.stat_current_HP -= amount;
+
+func _helper_clear_skill()->void:
+	self.skill_chosen = null;
--- scripts/character/characterData/characterClass.gd
+++ scripts/character/characterData/characterClass.gd
@@ -1,14 +1,9 @@
 class_name BattlePlayer
-extends Node2D
+extends BattleEntity
 
-@export var battle_data: statsResource;
 @export var battleText: quotesResource;
-@export var defensive: BattleSkill;
-
-@export var spriteSheet: AnimatedSprite2D;
 @export var animationEffects: AnimationPlayer;
 @export var selectionButton: Button;
-@export var dialouge: Label;
 
 func _ready() -> void:
 	signal_manager.hide_me.connect(_hide_self)
@@ -18,21 +13,6 @@ func _getStats():
 	return battle_data
 
 ### SETUP PHASE
-## Sets up a character for a turn starting
-func _battle_phase_setup()->void:
-	target = [];
-	is_defending = false;
-## The skill the battler uses.
-var skill_chosen:BattleSkill;
-## Uses an array to loop over multiple targets.
-var target = [];
-## Whether or not the character is defending or not.
-var is_defending: bool = false;
-## Checks if a character is battleReady
-func is_battle_ready()-> bool:
-	if (((self.skill_chosen != null) and !self.target.is_empty()) or is_defending):
-		return true
-	return false
 
 ### BATTLE PHASE
 func helper_battle_getBasePower()->int:
@@ -51,21 +31,6 @@ func _on_selection_pressed() -> void:
 	
 func _hide_self()->void:
 	if (self.scene_file_path != global_battle_information.currently_selected_hero.scene_file_path):
-		var _select_opacity_off = create_tween().tween_property(spriteSheet,"modulate:a",0.5,0.5);
+		var _select_opacity_off = create_tween().tween_property(entity_sprite_sheet,"modulate:a",0.5,0.5);
 	else:
-		var _select_opacity_on = create_tween().tween_property(spriteSheet,"modulate:a",1,0.5);
-
-### BATTLE PHASE
-func execute_skill()->void:
-	for hit in range(skill_chosen.hit_count):
-		var stat_choice = battle_data.source_stat_array[skill_chosen.stat_power];
-		var damage = skill_chosen._getDamageAmount(stat_choice);
-		for targeting in target:
-			target._change_hp(damage)
-			print(target.stat_current_hp)
-
-func _change_hp(amount:int)->void: 
-	battle_data.stat_current_HP -= amount;
-### HELPERS
-func _helper_clear_skill()->void:
-	self.skill_chosen = null;
+		var _select_opacity_on = create_tween().tween_property(entity_sprite_sheet,"modulate:a",1,0.5);
